34397cc6d48fd88258e9e6d6f7afc1b661efab6d
muhagung12 <muhagungstat@gmail.com>
2024-06-16 00:35:30 +0800

    Delete FinalProjectKelompok14 directory

--- FinalProjectKelompok14/spLocation.sql
+++
@@ -1,8 +0,0 @@
-CREATE PROCEDURE AddLocation (
-    @street_address VARCHAR(40),
-    @postal_code VARCHAR(12),
-    @city VARCHAR(30),
-    @state_province VARCHAR(25),
-    @country CHAR(3)
-)
-AS
