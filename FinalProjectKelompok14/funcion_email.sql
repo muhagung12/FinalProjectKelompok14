0eb9fb0bf2708d0e4caacd7d73e6457fa101759a
muhagung12 <muhagungstat@gmail.com>
2024-06-16 01:48:56 +0800

    Add files via upload

---
+++ FinalProjectKelompok14/funcion_email.sql
@@ -0,0 +1,11 @@
+CREATE FUNCTION isValidEmail(@email varchar(255))
+RETURNS bit AS
+BEGIN DECLARE @isValid bit;
+  SET @isValid = 0; --
+  IF (CHARINDEX('@', @email) > 0 AND CHARINDEX('.', @email) >
+  CHARINDEX('@', @email)) 
+  BEGIN SET @isValid = 1; 
+  END;
+  RETURN @isValid;
+END;
+GO
