c0a10c3963ec6e253443ebceb0069918ad88a4b3
gemmapraditya <gemmapraditya@gmail.com>
2024-06-17 23:11:25 +0700

    Update Permissions.sql

--- FinalProjectKelompok14/Database Employee/Permissions.sql
+++ FinalProjectKelompok14/Database Employee/Permissions.sql
@@ -2,3 +2,8 @@ CREATE ROLE super_admin;
 CREATE ROLE admin;
 CREATE ROLE manager;
 CREATE ROLE employee;
+
+-- Permissions for Super Admin
+GRANT INSERT, SELECT, UPDATE, DELETE ON tbl_employees TO super_admin;
+GRANT INSERT, UPDATE, DELETE ON tbl_jobs TO super_admin;
+GRANT INSERT, UPDATE, DELETE ON tbl_departments TO super_admin;
