1469f38f45f2c257e35638971853ecd3e3b86e44
gemmapraditya <gemmapraditya@gmail.com>
2024-06-17 23:18:14 +0700

    Update Permissions.sql

--- FinalProjectKelompok14/Database Employee/Permissions.sql
+++ FinalProjectKelompok14/Database Employee/Permissions.sql
@@ -14,5 +14,10 @@ GRANT INSERT, UPDATE, DELETE ON tbl_roles TO super_admin;
 GRANT INSERT, UPDATE, DELETE ON tbl_role_permissions TO super_admin;
 
 -- Permissions for Admin
-GRANT INSERT, SELECT, UPDATE, DELETE ON tbl_employees TO super_admin;
-GRANT INSERT, UPDATE, DELETE ON tbl_jobs TO super_admin;
+GRANT INSERT, SELECT, UPDATE, DELETE ON tbl_employees TO admin;
+GRANT INSERT, UPDATE, DELETE ON tbl_jobs TO admin;
+GRANT INSERT, UPDATE, DELETE ON tbl_departments TO admin;
+GRANT INSERT, UPDATE, DELETE ON tbl_locations TO admin;
+GRANT INSERT, UPDATE ON tbl_countries TO admin;
+GRANT INSERT, UPDATE ON tbl_regions TO admin;
+GRANT INSERT, UPDATE ON tbl_roles TO admin;
