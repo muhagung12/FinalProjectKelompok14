b28a3153b90386ec2fa74cf014d1bf69bd119772
adzkiahy <adzkiahermayulia08@gmail.com>
2024-06-18 10:27:44 +0700

    create profil

--- FinalProjectKelompok14/VW/vw_profil_details.sql
+++ FinalProjectKelompok14/VW/vw_profil_details.sql
@@ -1,9 +1,9 @@
 CREATE VIEW profile_details AS
 SELECT 
-  e.id AS employee_id, e.first_name, e.last_name, e.gender,
+  e.id AS employee_id, e.firstname, e.lastname, e.gender,
   e.email, e.phone, e.hire_date, e.salary,
-  m.first_name AS manager_first_name,  -- Assuming 'first_name' exists in tbl_employees
-  m.last_name AS manager_last_name,    -- Assuming 'last_name' exists in tbl_employees
+  m.firstname AS manager_first_name,  -- Assuming 'first_name' exists in tbl_employees
+  m.lastname AS manager_last_name,    -- Assuming 'last_name' exists in tbl_employees
   j.title AS job_title,  d.name AS department_name,l.city,
   l.state_province, c.name AS country_name FROM tbl_employees e
 LEFT JOIN tbl_employees m ON e.manager = m.id  -- Include manager details (can be NULL if no manager)
