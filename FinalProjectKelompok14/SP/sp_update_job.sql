a4ff0ccd6e38b2e6281359cfe8d069baa9c72bce
gemmapraditya <gemmapraditya@gmail.com>
2024-06-18 09:30:36 +0700

    Update sp_update_job.sql

--- FinalProjectKelompok14/SP/sp_update_job.sql
+++ FinalProjectKelompok14/SP/sp_update_job.sql
@@ -1,29 +1,39 @@
-CREATE PROCEDURE 
-updateJob (@id varchar(10),  @title varchar(35),  @min_salary int, @max_salary int
+CREATE PROCEDURE updateJob (
+  @job_id varchar(10),
+  @job_title varchar(35),
+  @job_min_salary int,
+  @job_max_salary int
 )
-AS BEGIN
-  DECLARE @errorMessage nvarchar(500);
+AS
+BEGIN
+  DECLARE @errorMsg nvarchar(500);
   DECLARE @rowsAffected int;
 
-  BEGIN TRY
-    IF @max_salary < @min_salary
-    BEGIN SET @errorMessage = 'Max salary cannot be less than min salary';
-      RAISERROR ('Error: %s', 16, 1, @errorMessage);
-    END
+  -- Check if max salary is less than min salary
+  IF @job_max_salary < @job_min_salary
+  BEGIN
+    SET @errorMsg = 'Max salary cannot be less than min salary';
+    PRINT @errorMsg;
+    RETURN;
+  END;
 
-    UPDATE tbl_jobs
-    SET title = @title,
-        min_salary = @min_salary,
-        max_salary = @max_salary
-    WHERE id = @id;
-    SET @rowsAffected = @@ROWCOUNT;
+  -- Update job details
+  UPDATE tbl_jobs
+  SET title = @job_title,
+      min_salary = @job_min_salary,
+      max_salary = @job_max_salary
+  WHERE id = @job_id;
 
-    IF @rowsAffected = 0
-    BEGIN RAISERROR ('Job with ID %s not found.', 10, 1, @id);
-    END
-  END TRY
+  -- Get the number of rows affected by the update
+  SET @rowsAffected = @@ROWCOUNT;
 
-  BEGIN CATCH SET @errorMessage = ERROR_MESSAGE();
-   RAISERROR ('Error updating job: %s', 16, 1, @errorMessage);
-  END CATCH;
+  -- Check if the job with given ID exists
+  IF @rowsAffected = 0
+  BEGIN
+    SET @errorMsg = 'Job with ID ' + @job_id + ' not found.';
+    PRINT @errorMsg;
+    RETURN;
+  END;
+
+  PRINT 'Job details updated successfully';
 END;
