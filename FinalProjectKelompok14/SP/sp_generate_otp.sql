7b043fe8f76a5f05aed2eef292dfe06acad83fc7
gemmapraditya <gemmapraditya@gmail.com>
2024-06-18 09:22:09 +0700

    Update sp_generate_otp.sql

--- FinalProjectKelompok14/SP/sp_generate_otp.sql
+++ FinalProjectKelompok14/SP/sp_generate_otp.sql
@@ -1,30 +1,29 @@
-CREATE PROCEDURE generateOtp (@userEmail VARCHAR(25))
+CREATE PROCEDURE generateOtp (@user_email varchar(25))
 AS
 BEGIN
-    DECLARE @userId INT;
-    DECLARE @otp INT;
-    DECLARE @expiryDate DATETIME;
+  DECLARE @errorMsg nvarchar(500);
+  DECLARE @userId int;
 
-    SELECT @userId = id 
-    FROM tbl_employees 
-    WHERE email = @userEmail;
+  -- Fetch the user ID
+  SELECT @userId = id FROM tbl_employees WHERE email = @user_email;
 
-    IF @userId IS NOT NULL
-    BEGIN
-        SET @otp = FLOOR(1000000 * RAND()) + 1;
+  IF @userId IS NOT NULL
+  BEGIN
+    DECLARE @otp int; 
+    SET @otp = FLOOR(1000000 * RAND()) + 1;
 
-        SET @expiryDate = DATEADD(MINUTE, 10, (SELECT is_expired FROM tbl_accounts WHERE id = @userId));
+    -- Update OTP and expiration time in tbl_accounts
+    UPDATE tbl_accounts
+    SET otp = @otp,
+        is_used = 0,
+        is_expired = DATEADD(minute, 10, GETDATE())
+    WHERE id = @userId;
 
-        UPDATE tbl_accounts
-        SET otp = @otp,
-            is_used = 0,
-            is_expired = @expiryDate
-        WHERE id = @userId;
-
-        PRINT 'Generated OTP: ' + CAST(@otp AS VARCHAR(6));
-    END
-    ELSE
-    BEGIN
-        PRINT 'Error: Account Not Registered!';
-    END;
+    PRINT 'Generated OTP: ' + CAST(@otp AS varchar(6));
+  END
+  ELSE
+  BEGIN
+    SET @errorMsg = 'Account Not Registered!';
+    PRINT @errorMsg;
+  END
 END;
