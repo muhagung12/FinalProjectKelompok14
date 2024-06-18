f1adabebc6989b8e31bb26b53d3150ca8e008737
gemmapraditya <gemmapraditya@gmail.com>
2024-06-18 09:36:50 +0700

    Update sp_delete_role.sql

--- FinalProjectKelompok14/SP/sp_delete_role.sql
+++ FinalProjectKelompok14/SP/sp_delete_role.sql
@@ -1,13 +1,22 @@
-CREATE PROCEDURE deleteRole (@Id INT)
+CREATE PROCEDURE deleteRole (
+  @role_id INT
+)
 AS
 BEGIN
-    DECLARE @rowsAffected INT;
+  DECLARE @rowsAffected INT;
+  DECLARE @errorMsg NVARCHAR(500);
 
-    DELETE FROM tbl_roles WHERE id = @Id;
-    SET @rowsAffected = @@ROWCOUNT;
+  -- Attempt to delete the role
+  DELETE FROM tbl_roles WHERE id = @role_id;
+  SET @rowsAffected = @@ROWCOUNT;
 
-    IF @rowsAffected = 0
-    BEGIN
-        PRINT 'Role with the specified ID was not found.';
-    END;
+  -- If no rows were affected, the role was not found
+  IF @rowsAffected = 0
+  BEGIN
+    SET @errorMsg = 'Role with ID ' + CAST(@role_id AS VARCHAR(10)) + ' not found.';
+    PRINT @errorMsg;
+    RETURN;
+  END;
+
+  PRINT 'Role deleted successfully';
 END;
