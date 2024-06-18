94952f9c5b3af057d971f42441854951bb24ed11
gemmapraditya <gemmapraditya@gmail.com>
2024-06-18 09:33:58 +0700

    Update sp_delete_job.sql

--- FinalProjectKelompok14/SP/sp_delete_job.sql
+++ FinalProjectKelompok14/SP/sp_delete_job.sql
@@ -1,14 +1,24 @@
-CREATE PROCEDURE deleteJob (@id varchar(10))
-AS BEGIN
-  DECLARE @errorMessage nvarchar(500);
+CREATE PROCEDURE deleteJob (
+  @job_id varchar(10)
+)
+AS
+BEGIN
   DECLARE @rowsAffected int;
- BEGIN TRY DELETE FROM tbl_jobs WHERE id = @id;
- SET @rowsAffected = @@ROWCOUNT;
- IF @rowsAffected = 0
- BEGIN
- RAISERROR ('Job with ID %s not found.', 10, 1, @id);
-      END;
-  END TRY
-  BEGIN CATCH SET @errorMessage = ERROR_MESSAGE(); RAISERROR ('Error deleting job: %s', 16, 1, @errorMessage);
-  END CATCH;
+  DECLARE @errorMsg nvarchar(500);
+
+  -- Delete job
+  DELETE FROM tbl_jobs WHERE id = @job_id;
+
+  -- Get the number of rows affected by the delete operation
+  SET @rowsAffected = @@ROWCOUNT;
+
+  -- Check if a job with the given ID was deleted
+  IF @rowsAffected = 0
+  BEGIN
+    SET @errorMsg = 'Job with ID ' + @job_id + ' not found.';
+    PRINT @errorMsg;
+    RETURN;
+  END;
+
+  PRINT 'Job deleted successfully';
 END;
