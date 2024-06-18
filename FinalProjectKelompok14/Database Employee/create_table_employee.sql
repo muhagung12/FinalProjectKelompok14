2b5ca5044643b9c5b2fa7eadc5f66e4c93a2e403
gemmapraditya <gemmapraditya@gmail.com>
2024-06-18 09:10:50 +0700

    Update create_table_employee.sql

--- FinalProjectKelompok14/Database Employee/create_table_employee.sql
+++ FinalProjectKelompok14/Database Employee/create_table_employee.sql
@@ -1,101 +1,107 @@
-USE employee;
-GO
-
-CREATE TABLE employees (
+CREATE TABLE tbl_employees (
   id INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
   firstname VARCHAR(25) NOT NULL,
-  lastname VARCHAR(25) ,
+  lastname VARCHAR(25),
   gender VARCHAR(10) NOT NULL,
   email VARCHAR(25) NOT NULL UNIQUE,
   phone VARCHAR(20),
   hire_date DATE NOT NULL,
   salary INT,
   manager INT,
-  FOREIGN KEY (manager) REFERENCES employee(id),
   job VARCHAR(10),
-  department INT,
-  FOREIGN KEY (job) REFERENCES job(name),
-  FOREIGN KEY (department) REFERENCES department(id)
+  department INT
 );
 
-CREATE TABLE jobs (
-id VARCHAR(10) PRIMARY KEY NOT NULL,
-title VARCHAR(35) NOT NULL,
-min_salary int DEFAULT 0,
-max_salary int DEFAULT 0
+CREATE TABLE tbl_jobs (
+  id VARCHAR(10) PRIMARY KEY NOT NULL,
+  title VARCHAR(35) NOT NULL,
+  min_salary INT DEFAULT 0,
+  max_salary INT DEFAULT 0
 );
 
-CREATE TABLE job_histories(
-employee int PRIMARY KEY NOT NULL,
-start_date DATE PRIMARY KEY NOT NULL,
-end_date DATE,
-status VARCHAR(10) NOT NULL,
-job VARCHAR(10) NOT NULL,
-department int NOT NULL,
-FOREIGN KEY (department) REFERENCES departments(id),
-FOREIGN KEY (job) REFERENCES jobs(id)
-);
-
-CREATE TABLE departments (
+CREATE TABLE tbl_departments (
   id INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
   name VARCHAR(30) NOT NULL,
-  location INT,
-  FOREIGN KEY (location) REFERENCES locations(id)
+  location INT
 );
 
-CREATE TABLE locations(
+CREATE TABLE tbl_locations (
   id INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
   street_address VARCHAR(40),
   postal_code VARCHAR(12),
   city VARCHAR(30) NOT NULL,
   state_province VARCHAR(25),
-  country CHAR(3),
-  FOREIGN KEY (country) REFERENCES countries(id)
+  country CHAR(3)
 );
 
-CREATE TABLE countries(
+CREATE TABLE tbl_countries (
   id CHAR(3) PRIMARY KEY NOT NULL,
   name VARCHAR(25) NOT NULL,
-	region INT NOT NULL,
-    FOREIGN KEY (region) REFERENCES regions(id)
+  region INT NOT NULL
 );
 
-CREATE TABLE regions(
-  id int PRIMARY KEY IDENTITY(1,1) NOT NULL,
+CREATE TABLE tbl_regions (
+  id INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
   name VARCHAR(25) NOT NULL
 );
 
-CREATE TABLE accounts (
-    id INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
-    username VARCHAR(25),
-	password VARCHAR(255) NOT NULL,
-	otp INT NOT NULL,
-	is_expired DATETIME NOT NULL,
-    is_used BIT DEFAULT 0,
-	FOREIGN KEY (id) REFERENCES employees(id)
+CREATE TABLE tbl_accounts (
+  id INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+  username VARCHAR(25),
+  password VARCHAR(255) NOT NULL,
+  otp INT NOT NULL,
+  is_expired DATETIME NOT NULL,
+  is_used BIT DEFAULT 0
 );
 
-CREATE TABLE account_roles (
-    id INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
-	account INT NOT NULL,
-    FOREIGN KEY (account) REFERENCES accounts(id),
-    role INT NOT NULL,
-	FOREIGN KEY (role) REFERENCES roles(id)
+CREATE TABLE tbl_account_roles (
+  id INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+  account INT NOT NULL,
+  role INT NOT NULL
 );
 
-CREATE TABLE roles(
-  id int PRIMARY KEY IDENTITY(1,1) NOT NULL,
+CREATE TABLE tbl_roles (
+  id INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
   name VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE role_permissions(
-  id int PRIMARY KEY IDENTITY(1,1) NOT NULL,
+CREATE TABLE tbl_role_permissions (
+  id INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
   role INT NOT NULL,
-  permission int NOT NULL
-  FOREIGN KEY (permission) REFERENCES permission(id)
+  permission INT NOT NULL
 );
 
-CREATE TABLE permissions(
-  id int PRIMARY KEY IDENTITY(1,1) NOT NULL,
+CREATE TABLE tbl_permissions (
+  id INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
   name VARCHAR(100) NOT NULL
 );
+
+ALTER TABLE tbl_employees
+ADD FOREIGN KEY (manager) REFERENCES tbl_employees(id);
+
+ALTER TABLE tbl_employees
+ADD FOREIGN KEY (job) REFERENCES tbl_jobs(id);
+
+ALTER TABLE tbl_employees
+ADD FOREIGN KEY (department) REFERENCES tbl_departments(id);
+
+ALTER TABLE tbl_departments
+ADD FOREIGN KEY (location) REFERENCES tbl_locations(id);
+
+ALTER TABLE tbl_countries
+ADD FOREIGN KEY (region) REFERENCES tbl_regions(id);
+
+ALTER TABLE tbl_accounts
+ADD FOREIGN KEY (id) REFERENCES tbl_employees(id);
+
+ALTER TABLE tbl_account_roles
+ADD FOREIGN KEY (account) REFERENCES tbl_accounts(id);
+
+ALTER TABLE tbl_account_roles
+ADD FOREIGN KEY (role) REFERENCES tbl_roles(id);
+
+ALTER TABLE tbl_role_permissions
+ADD FOREIGN KEY (role) REFERENCES tbl_roles(id);
+
+ALTER TABLE tbl_role_permissions
+ADD FOREIGN KEY (permission) REFERENCES tbl_permissions(id);
