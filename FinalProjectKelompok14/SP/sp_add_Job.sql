3e1355603809a973ea4a4ab98a41018408a0d8d6
gemmapraditya <gemmapraditya@gmail.com>
2024-06-18 09:27:27 +0700

    Update sp_add_Job.sql

--- FinalProjectKelompok14/SP/sp_add_Job.sql
+++ FinalProjectKelompok14/SP/sp_add_Job.sql
@@ -1,33 +1,22 @@
 CREATE PROCEDURE AddJob (
-  @id varchar(10),
-  @title varchar(35),
-  @min_salary int,
-  @max_salary int
+  @job_id varchar(10),
+  @job_title varchar(35),
+  @job_min_salary int,
+  @job_max_salary int
 )
 AS
 BEGIN
-  DECLARE @errorMessage nvarchar(500);
+  DECLARE @errorMsg nvarchar(500);
 
-  BEGIN TRY
-    IF @max_salary < @min_salary
-    BEGIN SET @errorMessage = 'Max salary cannot be less than min salary';
-    RAISERROR ('Error: %s', 16, 1, @errorMessage);
-    END
+  IF @job_max_salary < @job_min_salary
+  BEGIN
+    SET @errorMsg = 'Max salary cannot be less than min salary';
+    PRINT @errorMsg;
+    RETURN;
+  END;
 
-    INSERT INTO tbl_jobs (id, title, min_salary, max_salary)
-    VALUES (@id, @title, @min_salary, @max_salary);
-  END TRY
+  INSERT INTO tbl_jobs (id, title, min_salary, max_salary)
+  VALUES (@job_id, @job_title, @job_min_salary, @job_max_salary);
 
-  BEGIN CATCH SET @errorMessage = ERROR_MESSAGE();
-  RAISERROR ('Error adding job: %s', 16, 1, @errorMessage);
-  END CATCH;
+  PRINT 'Job added successfully';
 END;
-
-
-EXEC sp_helptext 'AddJob';
-EXEC AddJob 
-    @id = M001', 
-    @title = 'Software Developer', 
-    @min_salary = 50000, 
-    @max_salary = 100000;
-SELECT * FROM tbl_jobs WHERE id = 'M001';
