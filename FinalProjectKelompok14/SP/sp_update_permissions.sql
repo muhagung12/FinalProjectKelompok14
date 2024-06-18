25c9adcda4adbfe19bd09912604d78586c38288d
gemmapraditya <gemmapraditya@gmail.com>
2024-06-18 09:33:17 +0700

    Update sp_update_permissions.sql

--- FinalProjectKelompok14/SP/sp_update_permissions.sql
+++ FinalProjectKelompok14/SP/sp_update_permissions.sql
@@ -1,19 +1,25 @@
-CREATE PROCEDURE updatePermission (@id int, @name varchar(100)
+CREATE PROCEDURE updatePermission (
+  @perm_id int,
+  @new_name varchar(100)
 )
-AS BEGIN
-  DECLARE @errorMessage nvarchar(500);
+AS
+BEGIN
   DECLARE @rowsAffected int;
+  DECLARE @errorMsg nvarchar(500);
 
-  BEGIN TRY
-  UPDATE tbl_permissions 
-  SET name = @name
-  WHERE id = @id;
+  -- Update permission name
+  UPDATE tbl_permissions SET name = @new_name WHERE id = @perm_id;
+
+  -- Get the number of rows affected by the update
   SET @rowsAffected = @@ROWCOUNT;
+
+  -- Check if the permission with given ID exists
   IF @rowsAffected = 0
-  BEGIN RAISERROR ('Permission with ID %d not found.', 10, 1, @id);
-      END;
-  END TRY
-  BEGIN CATCH SET @errorMessage = ERROR_MESSAGE();
-  RAISERROR ('Error updating permission: %s', 16, 1, @errorMessage);
-  END CATCH;
+  BEGIN
+    SET @errorMsg = 'Permission with ID ' + CAST(@perm_id AS VARCHAR(10)) + ' not found.';
+    PRINT @errorMsg;
+    RETURN;
+  END;
+
+  PRINT 'Permission details updated successfully';
 END;
